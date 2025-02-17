7aa86d09dc6e2d02ada2e29ed7c847592197c8ea
Ash0709 <155084404+ashwini070@users.noreply.github.com>
2025-02-17 11:19:09 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+const express = require(`express`);
+const app = express();
+const port = 5000;
+
+app.get(`/`, (req, res) => {
+  res.send(`Backend is running`);
+});
+
+app.listen(port, () => {
+  console.log(`Backend server running at http://localhost:${port}`);
+});
